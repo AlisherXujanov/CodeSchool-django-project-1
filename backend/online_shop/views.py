51f826c384304e552b71497b5904231181bea8e8
Alisher <alisherxujanov163@gmail.com>
2026-03-06 18:24:04 +0500

    Add .gitignore for virtual environment and update README with setup instructions

--- backend/online_shop/views.py
+++ backend/online_shop/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    text = "This is test text"
+    context = {"text": text}
+    return render(request, "home.html", context)
+
+
+def about(request):
+    text = "This is test text for ABOUT PAGE"
+    context = {"text": text}
+    return render(request, "about.html", context)
+
+
+# request  =>  surov
+# response =>  javob
